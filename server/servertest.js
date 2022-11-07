e016167d9950ca31d4c2ae86ab1ce9aae0dccb92
IJon127 <ijon.hsieh@gmail.com>
2022-11-06 21:37:48 -0500

    add hostname

--- server/servertest.js
+++ server/servertest.js
@@ -1,11 +1,12 @@
 const http = require("http");
 
+const hostname = "localhost";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
-  res.end("No express.js!\n");
+  res.end("No express\n");
 });
 
 server.listen(port, hostname, () => {
