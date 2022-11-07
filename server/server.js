e78abd2273986a82eff81cfb5260a74195ca2d9e
IJon127 <ijon.hsieh@gmail.com>
2022-11-07 13:35:22 -0500

    add get root

--- server/server.js
+++ server/server.js
@@ -4,14 +4,24 @@ const app = express();
 
 const projects = JSON.parse(fs.readFileSync(`${__dirname}/data/projects.json`));
 
-app.get("/projects", (req, res) => {
+app.get("/", getRoot);
+app.get("/projects", getProjects);
+
+const port = 8000;
+app.listen(port, () => console.log(`Server started on port ${port}`));
+
+function getRoot(req, res) {
+  res.status(200).json({
+    status: "success",
+    data: "root",
+  });
+}
+
+function getProjects(req, res) {
   res.status(200).json({
     status: "success",
     data: {
       projects,
     },
   });
-});
-
-const port = 8000;
-app.listen(port, () => console.log(`Server started on port ${port}`));
+}
