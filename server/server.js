bb904ffc75b4491494e834dbf09ed30b386db998
IJon127 <ijon.hsieh@gmail.com>
2022-11-06 20:44:17 -0500

    move data dir

--- server/server.js
+++ server/server.js
@@ -2,9 +2,7 @@ const fs = require("fs");
 const express = require("express");
 const app = express();
 
-const projects = JSON.parse(
-  fs.readFileSync(`${__dirname}/bin/data/projects.json`)
-);
+const projects = JSON.parse(fs.readFileSync(`${__dirname}/data/projects.json`));
 
 app.get("/projects", (req, res) => {
   res.status(200).json({
