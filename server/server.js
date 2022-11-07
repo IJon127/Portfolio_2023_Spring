8a3d5ca26f5a7d47f7fb2a16703c229eac06b7c7
IJon127 <ijon.hsieh@gmail.com>
2022-11-07 16:58:22 -0500

    undo

--- server/server.js
+++ server/server.js
@@ -4,21 +4,29 @@ const app = express();
 
 const projects = JSON.parse(fs.readFileSync(`${__dirname}/data/projects.json`));
 
-app.get("/", getRoot);
-app.get("/projects", getProjects);
-
-const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
-
-function getRoot(req, res) {
-  res.status(200).send("Hi from root!");
-}
-
-function getProjects(req, res) {
+//app.get("/", getRoot);
+//app.get("/projects", getProjects);
+app.get("/projects", (req, res) => {
   res.status(200).json({
     status: "success",
     data: {
       projects,
     },
   });
-}
+});
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
+
+// function getRoot(req, res) {
+//   res.status(200).send("Hi from root!");
+// }
+
+// function getProjects(req, res) {
+//   res.status(200).json({
+//     status: "success",
+//     data: {
+//       projects,
+//     },
+//   });
+// }
