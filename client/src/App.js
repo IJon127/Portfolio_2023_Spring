7ae37f9789c2323e442edb4827e055025d817750
IJon127 <ijon.hsieh@gmail.com>
2022-12-27 22:45:16 -0500

    edit router

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-import React, { useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 
 import Header from "./components/layouts/header/Header";
@@ -6,6 +5,7 @@ import Main from "./components/layouts/main/Main";
 import Footer from "./components/layouts/footer/Footer";
 import Home from "./components/pages/home/Home";
 import Projects from "./components/pages/projects/Projects";
+import Project from "./components/pages/project/Project";
 import Exercises from "./components/pages/exercises/Exercises";
 import About from "./components/pages/about/About";
 
@@ -17,10 +17,11 @@ function App() {
       <Header />
       <Main>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route index element={<Home />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/exercises" element={<Exercises />} />
           <Route path="/about" element={<About />} />
+          <Route path="/projects/:id" element={<Project />} />
         </Routes>
       </Main>
       <Footer />
