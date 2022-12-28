d67779bc9620c39a142bd82c9dd07492c0af189d
IJon127 <ijon.hsieh@gmail.com>
2022-12-27 21:16:27 -0500

    use navlink

--- client/src/components/layouts/header/Navbar.jsx
+++ client/src/components/layouts/header/Navbar.jsx
@@ -1,38 +1,39 @@
 import "./Navbar.css";
-import NavBtn from "./NavBtn";
+//import NavBtn from "./NavBtn";
 import NavSocial from "./NavSocial";
-
-const navBtns = [
-  {
-    id: 0,
-    name: "projects",
-    link: "projects",
-  },
-  {
-    id: 1,
-    name: "exercises",
-    link: "exercises",
-  },
-  {
-    id: 2,
-    name: "papers",
-    link: "papers",
-  },
-  {
-    id: 3,
-    name: "about",
-    link: "about",
-  },
-];
-
-const createNavBtn = (navBtn) => {
-  return <NavBtn key={navBtn.id} name={navBtn.name} link={navBtn.link} />;
-};
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
+  const navBtns = [
+    {
+      id: 0,
+      name: "projects",
+      link: "projects",
+    },
+    {
+      id: 1,
+      name: "exercises",
+      link: "exercises",
+    },
+    {
+      id: 2,
+      name: "papers",
+      link: "papers",
+    },
+    {
+      id: 3,
+      name: "about",
+      link: "about",
+    },
+  ];
+
   return (
     <div className="navbar">
-      {navBtns.map(createNavBtn)}
+      {navBtns.map((navBtn) => (
+        <NavLink className="navbar__btn" key={navBtn.id} to={navBtn.link}>
+          {navBtn.name}
+        </NavLink>
+      ))}
       <NavSocial />
     </div>
   );
