6043366dcc00a023e395d517821eb4989d594ae6
IJon127 <ijon.hsieh@gmail.com>
2023-01-01 02:54:54 -0500

    add lightbox

--- client/src/components/pages/project/ProjectGallery.jsx
+++ client/src/components/pages/project/ProjectGallery.jsx
@@ -1,11 +1,21 @@
+import { useState } from "react";
 import "./ProjectGallery.css";
 import PictureBox from "../../ui/PictureBox";
+import ProjectLightBox from "./ProjectLightBox";
 
 function ProjectGallery({ project, gallery }) {
+  const [lightBoxOpened, setLightBoxOpened] = useState(false);
+  const [currentIndex, setCurrentIndex] = useState(null);
+
+  const clickImgHandler = (index) => {
+    setLightBoxOpened(true);
+    setCurrentIndex(index);
+  };
+
   return (
     <div className="project-gallery">
-      {gallery.map((pic) => (
-        <button key={pic}>
+      {gallery.map((pic, index) => (
+        <button key={index} onClick={() => clickImgHandler(index)}>
           <PictureBox
             className=""
             aspectRatio="1/1"
@@ -14,6 +24,15 @@ function ProjectGallery({ project, gallery }) {
           />
         </button>
       ))}
+      {lightBoxOpened && (
+        <ProjectLightBox
+          img={`/images/projects/${project}/${gallery[currentIndex]}`}
+          setLightBoxOpened={setLightBoxOpened}
+          setCurrentIndex={setCurrentIndex}
+          currentIndex={currentIndex}
+          arrayLength={gallery.length}
+        />
+      )}
     </div>
   );
 }
---
+++ client/src/components/pages/project/ProjectLightBox.jsx
@@ -0,0 +1,71 @@
+import { createPortal } from "react-dom";
+import { useEffect, useState } from "react";
+
+import "./ProjectLightBox.css";
+import Main from "../../layouts/main/Main";
+
+function ProjectLightBox({
+  className,
+  img,
+  setLightBoxOpened,
+  setCurrentIndex,
+  currentIndex,
+  arrayLength,
+}) {
+  const classes = "project-light-box " + className;
+  const [showBackArrow, setShowBackArrow] = useState(true);
+  const [showNextArrow, setShowNextArrow] = useState(true);
+
+  const clickArrowHandler = (increment) => {
+    const updateIndex = (currentIndex += increment);
+    setCurrentIndex(updateIndex);
+  };
+  useEffect(() => {
+    if (currentIndex === 0) {
+      setShowBackArrow(false);
+      setShowNextArrow(true);
+    } else if (currentIndex === arrayLength - 1) {
+      setShowBackArrow(true);
+      setShowNextArrow(false);
+    } else {
+      setShowBackArrow(true);
+      setShowNextArrow(true);
+    }
+  }, [currentIndex, arrayLength]);
+
+  return createPortal(
+    <div className={classes} scroll="no">
+      <Main className="project-light-box__close-btn-container">
+        <button
+          onClick={() => setLightBoxOpened(false)}
+          className="project-light-box__close-btn"
+        >
+          ✕
+        </button>
+      </Main>
+      <Main className="project-light-box__arrow-container">
+        {showBackArrow && (
+          <button
+            onClick={() => clickArrowHandler(-1)}
+            className="project-light-box__arrow"
+          >
+            &#8249;
+          </button>
+        )}
+        {showNextArrow && (
+          <button
+            onClick={() => clickArrowHandler(1)}
+            className="project-light-box__arrow project-light-box__arrow-next"
+          >
+            &#8250;
+          </button>
+        )}
+      </Main>
+      <img className="project-light-box__img" src={img} alt="" />
+    </div>,
+
+    document.getElementById("modal")
+  );
+}
+
+export default ProjectLightBox;
