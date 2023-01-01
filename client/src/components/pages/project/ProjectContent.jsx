3d912470a89b2e842571f600d54517f576bf9ca3
IJon127 <ijon.hsieh@gmail.com>
2023-01-01 03:00:24 -0500

    edit read more

--- client/src/components/pages/project/ProjectContent.jsx
+++ client/src/components/pages/project/ProjectContent.jsx
@@ -1,29 +1,58 @@
+import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 import "./ProjectContent.css";
 import ProjectInfo from "./ProjectInfo";
+import ProjectReadMore from "./ProjectReadMore";
 
 function ProjectContent({ project }) {
+  const [readMoreOpen, setReadMoreOpen] = useState(false);
+  const [readMoreBtn, setReadMoreBtn] = useState("read more →");
+
+  const readMoreHandler = () => {
+    setReadMoreOpen(!readMoreOpen);
+    if (readMoreBtn === "read more →") {
+      setReadMoreBtn("close X");
+    } else {
+      setReadMoreBtn("read more →");
+    }
+  };
+
   return (
     <div className="project-content">
       <ProjectInfo
         title={project.title}
         subtitle={project.subtitle}
         team={project.team}
+        company={project.company}
         types={project.types}
         time={project.time}
         location={project.location}
         venue={project.venue}
       />
 
-      <div className="project-content__description-container">
+      <div className="project-content__description">
         {project.description.map((para) => (
-          <p className="project-content__description" key={para}>
+          <p className="para" key={para}>
             {para}
           </p>
         ))}
       </div>
       {project.readMore && (
-        <button className="project-content__readMore-btn">read more →</button>
+        <div className="readMore-btn-container">
+          <button onClick={readMoreHandler} className="readMore-btn">
+            {readMoreBtn}
+          </button>
+        </div>
       )}
+      <motion.div layout className="project-content__readMore">
+        <AnimatePresence>
+          {project.readMore &&
+            readMoreOpen &&
+            project.readMore.map((readMore, index) => (
+              <ProjectReadMore key={index} readMore={readMore} />
+            ))}
+        </AnimatePresence>
+      </motion.div>
     </div>
   );
 }
