f75f28933b7be95056a9b83af286069529d102a4
IJon127 <ijon.hsieh@gmail.com>
2023-01-01 03:01:27 -0500

    clean up css

--- client/src/components/pages/project/ProjectInfo.jsx
+++ client/src/components/pages/project/ProjectInfo.jsx
@@ -1,20 +1,26 @@
 import "./ProjectInfo.css";
 
-function ProjectInfo({ title, subtitle, team, types, time, location, venue }) {
+function ProjectInfo({
+  title,
+  subtitle,
+  team,
+  company,
+  types,
+  time,
+  location,
+  venue,
+}) {
   return (
     <div className="project-info">
       <h1 className="project-info__title">{title}</h1>
       <h3 className="project-info__subtitle">{subtitle}</h3>
-      <div>
-        {team && (
-          <p className="project-info__content">{`Co-created with ${team.join(
-            " & "
-          )}`}</p>
-        )}
-        <p className="project-info__content">{types.join(" / ")}</p>
-        <p className="project-info__content">{time}</p>
-        {location && <p className="project-info__content">{location}</p>}
-        {venue && <p className="project-info__content">{venue}</p>}
+      <div className="project-info__content">
+        {team && <p>{`Co-created with ${team.join(" & ")}`}</p>}
+        {company && <p>{`Worked at ${company}`}</p>}
+        <p>{types.join(" / ")}</p>
+        <p>{time}</p>
+        {location && <p>{location}</p>}
+        {venue && <p>{venue}</p>}
       </div>
     </div>
   );
